eb8460c40e60d7943f321ef192370544693325a3
Vamsi Krishna Valluru <grasscannoli@gmail.com>
2021-06-07 19:27:13 +0530

    Latency generator 2.0 and tester class for doing inference, getting latency, temp and evaluation metric

--- latency_dataset_generator.py
+++ latency_dataset_generator.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import time
-from prepare_task import GlueTask
+from tasks.glue.prepare_task import GlueTask
 import argparse
 import torch
 from torch.utils.data import DataLoader
@@ -16,242 +16,96 @@ from transformers import (
     set_seed,
     AutoConfig,
 )
-from custom_bert import BertForSequenceClassification
+from custom_layers.custom_bert import BertForSequenceClassification
 import os
 from pprint import pprint
 import pandas as pd
+from tester import Tester
 
 class LatencyDatasetGenerator():
-    def __init__(
-            self,
-            datasize=2000,
-            task = "sst2",
-            model_name_or_path = "bert-base-uncased",
-            use_pretrained_supertransformer = False,  
-            output_dir = "checkpoints",
-            max_seq_length = 128,
-            per_gpu_eval_batch_size = 64,
-            gradient_accumulation_steps = 1,
-            fp16 = True,
-            cpu = False,
-            seed = 42
-        ):
+    def __init__(self, path='./latency_dataset/sst2_gpu_gtx1080_tmp.csv', data_size=1000):
+        self.tester = Tester()
+        self.num_sub = data_size
+        self.num_supertransformer_layers = self.tester.get_supertransformer_config().sample_num_hidden_layers
 
-        # Initialize accelerator
-        accelerator = Accelerator(fp16=fp16, cpu=cpu)
-
-        print("Running on: ", accelerator.device)
-
-        # for now correcting adam bias is hardcoded to True
-        correct_bias = True
-        seed = int(seed)
-        per_gpu_eval_batch_size = int(per_gpu_eval_batch_size)
-
-        set_seed(seed)
-        config = self.get_supertransformer_config()
-        use_pretained = use_pretrained_supertransformer
-        model_checkpoint = model_name_or_path
-
-        # if this is not a path to a saved model checkpoint, ensure that we are using a bert-base model
-        if not os.path.exists(model_checkpoint):
-            assert (
-                model_checkpoint == "bert-base-cased"
-                or model_checkpoint == "bert-base-uncased"
-            ), f"HF model {model_checkpoint} is not supported, pls use bert-base"
-
-        glue_task = GlueTask(
-            task, model_checkpoint, config, initialize_pretrained_model=use_pretained
-        )
-
-        def collate_fn(examples):
-            # On TPU it's best to pad everything to the same length or training will be very slow.
-            if accelerator.distributed_type == DistributedType.TPU:
-                return glue_task.tokenizer.pad(
-                    examples,
-                    padding="max_length",
-                    max_length=max_seq_length,
-                    return_tensors="pt",
-                )
-            return glue_task.tokenizer.pad(examples, padding="longest", return_tensors="pt")
-
-        # Instantiate dataloader
-        eval_dataloader = DataLoader(
-            glue_task.eval_dataset,
-            shuffle=False,
-            collate_fn=collate_fn,
-            batch_size=per_gpu_eval_batch_size,
-        )
-
-        model = glue_task.model
-        metric = glue_task.metric
-        model = model.to(accelerator.device)
-
-        self.model = model
-        self.accelerator = accelerator
-        self.eval_dataloader = eval_dataloader
-        self.metric = metric
-        self.task = task
-
-        # The dataset to be stored:
-        self.num_sub = datasize
-        self.dataset = []
-
-
-    # Returns the search space from which
-    # sub-transformers are sampled:
-    def get_choices(self):
-        choices = {
-            "sample_hidden_size": [360, 480, 540, 600, 768],
-            "sample_num_attention_heads": [2, 4, 6, 8, 10, 12],
-            "sample_intermediate_size": [512, 1024, 2048, 3072],
-            "sample_num_hidden_layers": [6, 8, 10, 12],
-        }
-        return choices
-    
-    # Returns the Bert-Supertransformer Configuration:
-    def get_supertransformer_config(self):
-        config = AutoConfig.from_pretrained("bert-base-uncased")
-        config.sample_hidden_size = config.hidden_size
-        config.sample_num_hidden_layers = config.num_hidden_layers
-        config.sample_num_attention_heads = [
-            config.num_attention_heads
-        ] * config.sample_num_hidden_layers
-        config.sample_intermediate_size = [
-            config.intermediate_size
-        ] * config.sample_num_hidden_layers
-
-        return config
-
-    # Samples a subtransformer using given random seed:
-    def sample_subtransformer(self, randomize=False, rand_seed=0):
-        if randomize:
-            random.seed(rand_seed)
-        choices = self.get_choices()
-        config = self.get_supertransformer_config()
-
-        ### Figuring the number of hidden layers
-        hidden_layers_list = choices["sample_num_hidden_layers"]
-        num_hidden_layers = random.choice(hidden_layers_list)
-        setattr(config, "sample_num_hidden_layers", num_hidden_layers)
-
-        ## Figuring the hidden size for BERT embeddings
-        hidden_size_embeddings_list = choices["sample_hidden_size"]
-        num_hidden_size = random.choice(hidden_size_embeddings_list)
-        setattr(config, "sample_hidden_size", num_hidden_size)
-
-        config_dict = {
-            "sample_num_attention_heads": [],
-            "sample_intermediate_size": [],
-        }
-
-        for i in range(num_hidden_layers):
-            while True:
-                for key in config_dict.keys():
-
-                    choice_list = choices[key]
-                    choice = random.choice(choice_list)
-                    config_dict[key].append(choice)
-
-                if config.sample_hidden_size % config_dict["sample_num_attention_heads"][i]:
-                    for key in config_dict.keys():
-                        config_dict[key] = config_dict[key][:-1]
-                    continue
-                else:
-                    break
-
-        for key in config_dict.keys():
-            setattr(config, key, config_dict[key])
-
-        return config
-
-    # Function to get Latency and Eval metrics:
-    def get_latency_eval_subtransformer(
-        self, model, config, eval_dataloader, accelerator, metric, task="sst2"
-    ):
-        # Set the subtransformer configuration:
-        model.set_sample_config(config=config)
-
-        # Start measuring the Latency:
-        start_time = torch.cuda.Event(enable_timing=True)
-        end_time = torch.cuda.Event(enable_timing=True)
-        start_time.record()
-
-        # Inference of subtransformer:
-        model.eval()
-        for step, batch in enumerate(eval_dataloader):
-            # We could avoid this line since we set the accelerator with `device_placement=True`.
-            batch.to(accelerator.device)
-            with torch.no_grad():
-                outputs = model(**batch)
-            if task == "stsb":
-                predictions = predictions[:, 0]
-            else:
-                predictions = outputs.logits.argmax(dim=-1)
-            metric.add_batch(
-                predictions=accelerator.gather(predictions),
-                references=accelerator.gather(batch["labels"]),
-            )
-        eval_metric = metric.compute()
-
-        # End measuring the Latency:
-        end_time.record()
-        torch.cuda.synchronize()
-        latency = start_time.elapsed_time(end_time)/1000
-
-        return eval_metric, latency
+        # make columns of csv:
+        self.columns = [
+            'encoder_embed_dim',
+            'encoder_layer_num',
+        ]
+        for i in range(self.num_supertransformer_layers):
+            self.columns.append(f'encoder_ffn_embed_dim_{i}')
+        for i in range(self.num_supertransformer_layers):
+            self.columns.append(f'encoder_self_attention_heads_{i}')
+        self.columns += [
+            'latency',
+            'temp',
+        ]
+        # self.columns += [
+        #     'latency_mean_encoder',
+        #     'latency_std_encoder',
+        #     'temp_mean_encoder',
+        #     'temp_std_encoder',
+        # ]
+        self.path = path
     
     # Function to generate a latency dataset:
     def generate(self):
         # Measure Average Latency for different sub-transformers:
         avg_iter = 30
-        for subNo in tqdm(range(self.num_sub)):
+        for subNo in range(self.num_sub):
+            print(f'Evaluating subtransformer number {subNo}')
             lat_list = []
+            temp_list = []
 
             # Sample subtransformer to measure latency for:
             random_number = int(10000 * time.perf_counter())
-            sampled_config = self.sample_subtransformer(
+            sampled_config = self.tester.sample_subtransformer(
                 randomize=True,
                 rand_seed=random_number
             )
 
-            for _ in range(avg_iter):
-                eval_metric, latency = self.get_latency_eval_subtransformer(
-                    self.model, sampled_config, self.eval_dataloader, self.accelerator, self.metric, task=self.task
-                )
+            for iter in tqdm(range(avg_iter)):
+                latency, temp = self.tester.lat_data_point(sampled_config)
                 lat_list.append(latency)
+                temp_list.append(temp)
 
-            avg_latency = np.mean(np.array(lat_list))
-            std_latency = np.std(np.array(lat_list))
+                avg_latency = np.mean(np.array(lat_list))
+                std_latency = np.std(np.array(lat_list))
+                avg_temp = np.mean(np.array(temp_list))
+                std_temp = np.std(np.array(temp_list))
 
-            # Create a datapoint in dataset
-            # The first four columns correspond to config:
-            # encoder_embed_dim, encoder_layer_num, encoder_ffn_embed_dim, encoder_self_attention_heads
-            # The last two columns correspond to the avg latency and std dev
-            row = []
-            row.append(sampled_config.sample_hidden_size)
-            row.append(sampled_config.sample_num_hidden_layers)
-            row.append(np.mean(np.array(sampled_config.sample_intermediate_size)))
-            row.append(np.mean(np.array(sampled_config.sample_num_attention_heads)))
-            row.append(avg_latency)
-            row.append(std_latency)
-            self.dataset.append(row)
+                # Create a datapoint in dataset
+                # The first four columns correspond to config:
+                # encoder_embed_dim, encoder_layer_num, encoder_ffn_embed_dim, encoder_self_attention_heads
+                # The last two columns correspond to the avg latency and std dev
+                row = []
+                row.append(sampled_config.sample_hidden_size)
+                row.append(sampled_config.sample_num_hidden_layers)
+                row += self.encode(sampled_config.sample_intermediate_size)
+                row += self.encode(sampled_config.sample_num_attention_heads)
+                row.append(latency)
+                row.append(temp)
+                # row.append(avg_latency)
+                # row.append(std_latency)
+                # row.append(avg_temp)
+                # row.append(std_temp)
+                self.writeRow(row, first_line_flag = (subNo > 0 or iter > 0))
     
     # Function to write dataset to a csv file:
-    def writeData(self, path='./latency_dataset/sst2_gpu_gtx1080.csv'):
-        data = np.array(self.dataset)
-        columns = [
-            'encoder_embed_dim',
-            'encoder_layer_num',
-            'encoder_ffn_embed_dim_avg',
-            'encoder_self_attention_heads_avg',
-            'latency_mean_encoder',
-            'latency_std_encoder'
-        ]
-        pd.DataFrame(data=data, columns=columns).to_csv(path, index=False)
+    def writeRow(self, row, first_line_flag = 0):
+        data = np.array([row])
+        if first_line_flag == 0:
+            pd.DataFrame(data=data, columns=self.columns).to_csv(self.path, index=False)
+        else:
+            pd.DataFrame(data=data, columns=self.columns).to_csv(self.path, index=False, header=False, mode='a')
+    
+    def encode(self, lst):
+        extra = self.num_supertransformer_layers-len(lst)
+        return lst + [-1]*extra
 
 
-        
-
-ldg = LatencyDatasetGenerator(datasize=1000)
-ldg.generate()
-ldg.writeData('./latency_dataset/sst2_gpu_gtx1080_part_2.csv')
+if __name__ == "__main__":
+    os.environ["TOKENIZERS_PARALLELISM"]='false'
+    ldg = LatencyDatasetGenerator(path = './latency_dataset/sst2_gpu_gtx1080_final_2.csv', data_size=1000)
+    ldg.generate()
